be1d148ca742780e27e4c9b1da9d887fa40956c4
Emily Brown <emily_brown@rogers.com>
2019-06-11 18:41:20 -0400

    fixed function to ensure scoreboard values were tallying

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
 
-// Create Playernumber display
-let playerNumber = 0;
-$("#playerNumber").text(playerNumber);
-
 // Create random number generator to pick rnadom number anywhere from 19 - 120
 
 $("#randomButton").on("click", function(){
-    let random = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    $("#randomNumber").text(random);
+    console.log("clicked")
+    let computerNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    $("#computerNumber").text(computerNumber);
 });
 
 // Create button hidden value randomization
@@ -17,66 +14,104 @@ let button2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 let button3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 let button4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 
+// Set Scoreboard to 0 to begin game with
+
+let winsTracker = 0;
+$("#winsTracker").text(winsTracker);
+
+let lossesTracker = 0;
+$("#lossesTracker").text(lossesTracker)
+
+// Set "Your Number" to 0 to begin game with
+let playerNumber = 0;
+$("#playerNumber").text(playerNumber); 
+
+// Create win function 
+function winning() {
+    winsTracker = winsTracker + 1;
+    $("#winsTracker").text(winsTracker);
+    reset();
+    }
+
+// Create lose function
+function losing() {
+    lossesTracker = lossesTracker + 1;
+    $("#lossesTracker").text(lossesTracker);
+    reset();
+    }
+
 // Create click function for each of the buttons so that when the button is clicked, each will hold a different hidden random value and that random value gets added to the "Your Number" tally
 
 $("#button1").on("click", function(){
         playerNumber = playerNumber + button1;
+        let compNumber = $('#computerNumber').text()
         $("#playerNumber").text(playerNumber);
-        if (playerNumber === random) {
+        console.log(playerNumber, parseInt(compNumber))
+        if (playerNumber === parseInt(compNumber)) {
             winning();
-        }
-        else (playerNumber > random) ;{
+            console.log("win!")
+            // reset();
+        //     resetPlayerNumber();
+        //     resetRandomButton();
+        //     resetButtons();
+        } else if (playerNumber > parseInt(compNumber)) {
             losing();
+            console.log("lose!")
         }
 });
 
 $("#button2").on("click", function(){
-        playerNumber = playerNumber + button2;
-            $("#playerNumber").text(playerNumber)
+    playerNumber = playerNumber + button2;
+    let compNumber = $('#computerNumber').text()
+    $("#playerNumber").text(playerNumber);
+    console.log(playerNumber, parseInt(compNumber))
+    if (playerNumber === parseInt(compNumber)) {
+        winning();
+        console.log("win!")
+    } else if (playerNumber > parseInt(compNumber)) {
+        losing();
+        console.log("lose!")
+    }
 });
 
 $("#button3").on("click", function(){
-        playerNumber = playerNumber + button3;
-            $("#playerNumber").text(playerNumber)
+    playerNumber = playerNumber + button3;
+    let compNumber = $('#computerNumber').text()
+    $("#playerNumber").text(playerNumber);
+    console.log(playerNumber, parseInt(compNumber))
+    if (playerNumber === parseInt(compNumber)) {
+        winning();
+        console.log("win!")
+    } else if (playerNumber > parseInt(compNumber)) {
+        losing();
+        console.log("lose!")
+    }
 });
 
 $("#button4").on("click", function(){
-        playerNumber = playerNumber + button4;
-            $("#playerNumber").text(playerNumber)
+    playerNumber = playerNumber + button4;
+    let compNumber = $('#computerNumber').text()
+    $("#playerNumber").text(playerNumber);
+    console.log(playerNumber, parseInt(compNumber))
+    if (playerNumber === parseInt(compNumber)) {
+        winning();
+        console.log("win!")
+    } else if (playerNumber > parseInt(compNumber)) {
+        losing();
+        console.log("lose!")
+    }
 });
 
-// Create Scoreboard values and functions
-let win = 0;
-let lose = 0;
+function reset() {
+    playerNumber = 0;
+    $("#playerNumber"). text(playerNumber);
+    computerNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    $("#computerNumber").text(computerNumber);
+    button1 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    button2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    button3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    button4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 
-function winning() {
-    win++;
-    $("#winsTracker").text(win);
-}
-
-function losing() {
-    lose++;
-    $("#lossesTracker").text(lose);
 }
 
 });
-
-// let button1 = Math.floor(Math.floor() * (12 - 1 + 1)) + 1;
-
-// $("")
-
-// let value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-//     for (let i = 0; i < value.length; i++) {
-//         let buttonValue = $(".buttontest");
-//         buttonValue.attr("data-value", value[i])
-//         $(".buttontest").append(buttonValue);}
-
-// $(".buttontest")on.("click", function() {
-//     let playerValue = ($(this).attr("data-value"));
-//     playerValue = parseInt(playerValue);
-//     $("#playerNumber").text(playerValue);
-
-
-
-
-// })
